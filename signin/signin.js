c22d7fd7fa70471204633c0770705fba921878d7
mahdiharish <mahdiharish12@gmail.com>
2023-03-17 17:49:09 +0800

    wefwefs

--- signin/signin.js
+++ signin/signin.js
@@ -1,31 +1,53 @@
-// CHECK EMAIL
-document.getElementById("emailSignIn").addEventListener(
-  "input",
-  (checkEmail = () => {
-    const emailSignIn = document.getElementById("emailSignIn").value;
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+// CHECK EMAIL BY INPUT
+const checkRegisterEmail = () => {
+  const emailSignUp = document.getElementById("registerEmail").value;
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(emailSignUp)) {
+    document.getElementById("emailValidation").innerHTML = "Invalid email!";
+  } else {
+    document.getElementById("emailValidation").innerHTML = "";
+  }
+};
+document
+  .getElementById("registerEmail")
+  .addEventListener("input", checkRegisterEmail);
 
-    if (!emailRegex.test(emailSignIn)) {
-      document.getElementById("emailValidation").innerHTML = "Invalid email!";
-    } else {
-      document.getElementById("emailValidation").innerHTML = "";
-    }
-  })
-);
+// CHECK PASSWORD BY INPUT
+const checkRegisterPassword = () => {
+  const passwordSignUp = document.getElementById("registerPassword").value;
+  const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$/;
+  if (!passwordRegex.test(passwordSignUp)) {
+    document.getElementById("pwValidation").innerHTML =
+      "Must contain at least 8 characters, one uppercase, lowercase, number, and symbol!";
+  } else {
+    document.getElementById("pwValidation").innerHTML = "";
+  }
+};
+document
+  .getElementById("registerPassword")
+  .addEventListener("input", checkRegisterPassword);
 
-// CHECK PASSWORD
-document.getElementById("pwSignIn").addEventListener(
-  "input",
-  (checkPassword = () => {
-    const passwordSignIn = document.getElementById("pwSignIn").value;
-    const passwordRegex =
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$/;
-
-    if (!passwordRegex.test(passwordSignIn)) {
-      document.getElementById("pwValidation").innerHTML =
-        "Must contain at least 8 characters, one uppercase, lowercase, number, and symbol!";
-    } else {
-      document.getElementById("pwValidation").innerHTML = "";
-    }
-  })
-);
+// REGISTER NEW USER
+const userList = JSON.parse(localStorage.getItem("users")) || [
+  { email: "admin@gmail.com", password: "Admin123!" },
+];
+document.getElementById("registerBtn").addEventListener("click", function (e) {
+  e.preventDefault();
+  const email = document.getElementById("registerEmail");
+  const password = document.getElementById("registerPassword");
+  let validEmail = true;
+  let validPassword = true;
+  checkRegisterEmail();
+  checkRegisterPassword();
+  if (document.getElementById("emailValidation").innerHTML !== "") {
+    validEmail = false;
+  }
+  if (document.getElementById("pwValidation").innerHTML !== "") {
+    validPassword = false;
+  }
+  if (validEmail && validPassword) {
+    userList.push({ email: email.value, password: password.value });
+    localStorage.setItem("users", JSON.stringify(userList));
+    window.location.href = "./signin/signin.html";
+  }
+});
--- signup.js
+++ signup.js
@@ -1,34 +1,31 @@
-// CHECK EMAIL
-document.getElementById("registerEmail").addEventListener(
-  "input",
-  (checkRegisterEmail = () => {
-    const emailSignUp = document.getElementById("registerEmail").value;
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+// CHECK EMAIL BY INPUT
+const checkRegisterEmail = () => {
+  const emailSignUp = document.getElementById("registerEmail").value;
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(emailSignUp)) {
+    document.getElementById("emailValidation").innerHTML = "Invalid email!";
+  } else {
+    document.getElementById("emailValidation").innerHTML = "";
+  }
+};
+document
+  .getElementById("registerEmail")
+  .addEventListener("input", checkRegisterEmail);
 
-    if (!emailRegex.test(emailSignUp)) {
-      document.getElementById("emailValidation").innerHTML = "Invalid email!";
-    } else {
-      document.getElementById("emailValidation").innerHTML = "";
-    }
-  })
-);
-
-// CHECK PASSWORD
-document.getElementById("registerPassword").addEventListener(
-  "input",
-  (checkRegisterPassword = () => {
-    const passwordSignUp = document.getElementById("registerPassword").value;
-    const passwordRegex =
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$/;
-
-    if (!passwordRegex.test(passwordSignUp)) {
-      document.getElementById("pwValidation").innerHTML =
-        "Must contain at least 8 characters, one uppercase, lowercase, number, and symbol!";
-    } else {
-      document.getElementById("pwValidation").innerHTML = "";
-    }
-  })
-);
+// CHECK PASSWORD BY INPUT
+const checkRegisterPassword = () => {
+  const passwordSignUp = document.getElementById("registerPassword").value;
+  const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$/;
+  if (!passwordRegex.test(passwordSignUp)) {
+    document.getElementById("pwValidation").innerHTML =
+      "Must contain at least 8 characters, one uppercase, lowercase, number, and symbol!";
+  } else {
+    document.getElementById("pwValidation").innerHTML = "";
+  }
+};
+document
+  .getElementById("registerPassword")
+  .addEventListener("input", checkRegisterPassword);
 
 // REGISTER NEW USER
 const userList = JSON.parse(localStorage.getItem("users")) || [
@@ -40,20 +37,13 @@ document.getElementById("registerBtn").addEventListener("click", function (e) {
   const password = document.getElementById("registerPassword");
   let validEmail = true;
   let validPassword = true;
-  if (!checkRegisterEmail(email)) {
-    document.getElementById("emailValidation").innerHTML = "Invalid email!";
+  checkRegisterEmail();
+  checkRegisterPassword();
+  if (document.getElementById("emailValidation").innerHTML !== "") {
     validEmail = false;
-  } else {
-    document.getElementById("emailValidation").innerHTML = "";
-    validEmail = true;
   }
-  if (!checkRegisterPassword(password)) {
-    document.getElementById("passwordValidation").innerHTML =
-      "Must contain at least 8 characters, one uppercase, lowercase, number, and symbol!";
+  if (document.getElementById("pwValidation").innerHTML !== "") {
     validPassword = false;
-  } else {
-    document.getElementById("passwordValidation").innerHTML = "";
-    validPassword = true;
   }
   if (validEmail && validPassword) {
     userList.push({ email: email.value, password: password.value });
